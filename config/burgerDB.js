0b05c44021a9f2efb7de760d2d7fc4b3d01adf74
Joseph Farrish <joseph_a_f@hotmail.com>
2020-09-13 20:58:42 -0700

    struggleing with heroku

--- config/burgerDB.js
+++ config/burgerDB.js
@@ -1,39 +1,39 @@
 const mysql = require("mysql");
-const { connect } = require("../controllers/BurgerControl");
+//const { connect } = require("../controllers/BurgerControl");
 
-// const burgerDB = mysql.createConnection(
-//     {
-//         host:"localhost",
-//         port: 3306,
-//         user: "root",
-//         password: "enter101",
-//         database: "burgers_db"
-//     }
-// );
-
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-    connection = mysql.createConnection({
+const burgerDB = mysql.createConnection(
+    {
         host:"localhost",
         port: 3306,
         user: "root",
         password: "enter101",
-        database: "burger_db"
-    });
-};
+        database: "burgers_db"
+    }
+);
+
+// if(process.env.JAWSDB_URL){
+//     const burgerDB = mysql.createConnection(process.env.JAWSDB_URL);
+// }else{
+//   const burgerDB = mysql.createConnection({
+//         host:"localhost",
+//         port: 3306,
+//         user: "root",
+//         password: "enter101",
+//         database: "burger_db"
+//     });
+// };
 
 
 
-// //make the connection...
-// burgerDB.connect(function(err){
-//     if(err){
-//         console.error("error connectiong: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + burgerDB.threadId);
-// });
-
-connection.connect();
+//make the connection...
+burgerDB.connect(function(err){
+    if(err){
+        console.error("error connectiong: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + burgerDB.threadId);
+});
 
 module.exports = burgerDB;
+
+// module.exports = burgerDB;

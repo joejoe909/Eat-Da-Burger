7575a1399d0f5bf00deaa9b16d810e1aab6224dd
Joseph Farrish <joseph_a_f@hotmail.com>
2020-09-13 17:13:11 -0700

    adding heroku code

--- config/burgerDB.js
+++ config/burgerDB.js
@@ -1,22 +1,39 @@
 const mysql = require("mysql");
+const { connect } = require("../controllers/BurgerControl");
 
-const burgerDB = mysql.createConnection(
-    {
+// const burgerDB = mysql.createConnection(
+//     {
+//         host:"localhost",
+//         port: 3306,
+//         user: "root",
+//         password: "enter101",
+//         database: "burgers_db"
+//     }
+// );
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
         host:"localhost",
         port: 3306,
         user: "root",
         password: "enter101",
-        database: "burgers_db"
-    }
-);
+        database: "burger_db"
+    });
+};
 
-//make the connection...
-burgerDB.connect(function(err){
-    if(err){
-        console.error("error connectiong: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + burgerDB.threadId);
-});
+
+
+// //make the connection...
+// burgerDB.connect(function(err){
+//     if(err){
+//         console.error("error connectiong: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + burgerDB.threadId);
+// });
+
+connection.connect();
 
 module.exports = burgerDB;
